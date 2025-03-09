761ac499d3685b999509652efb037871ed796a17
Yashwanth <yashwanth5025@gmail.com>
2025-03-09 14:37:17 +0530

    Database schema

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,24 @@
+"use client"
+
 import React from 'react'
 import { EmptyOrg } from './_components/empty-org'
+import { useOrganization } from '@clerk/clerk-react'
+import { BoardList } from './_components/board-list'
 
-const DashboardPage = () => {
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string 
+  }
+}
+
+const DashboardPage = ({searchParams}: DashboardPageProps) => {
+  const { organization } = useOrganization()
   return (
     <div className='flex-1 h-[calc(100%-80px)] p-6'>
-        <EmptyOrg />
+        {!organization ? <EmptyOrg /> : ( 
+          <BoardList orgId={organization.id} query={searchParams} />
+        )}
     </div>
   )
 }
